4bd066fcfec89926324693db0d2973f717368cc5
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-08-18 11:26:42 -0400

    Comments and changed it to return result

--- Exercise Files/04_05/script.js
+++ Exercise Files/04_05/script.js
@@ -1,2 +1,17 @@
 //anonymous functions
 
+var a = 241/4;
+var b = 44/4;
+
+var theBiggest = function() { //this is the anonymous function placed into a var
+    var result;
+    a > b ? result = ["a", a] : result = ["b", b];
+
+    //console.log(result);
+
+    return result;
+}
+
+//theBiggest();
+
+console.log(theBiggest());
