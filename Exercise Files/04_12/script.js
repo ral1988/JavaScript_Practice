d1658c2bab0ce2f4f58f242e74c118d4b12acdf0
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-08-18 15:35:03 -0400

    Completed Closure lesson

--- Exercise Files/04_12/script.js
+++ Exercise Files/04_12/script.js
@@ -1,11 +1,34 @@
 function doSomeMath() {
 	var a = 5;
 	var b = 4;
-	var sum = a + b;
+	function multiply() {
+        var result = a * b;
+        return result;
+    }
 
-	return sum;
+    return multiply();
 }
 
 var theResult = doSomeMath();
 
 console.log("The result: ", theResult);
+
+function giveMeEms(pixels) {
+    var baseValue = 16;
+
+    function doTheMath() {
+        return pixels / baseValue;
+    }
+
+    return doTheMath;
+}
+
+var smallSize = giveMeEms(12);
+var mediumSize = giveMeEms(18);
+var largeSize = giveMeEms(24);
+var xLargeSize = giveMeEms(32);
+
+console.log("Small Size", smallSize());
+console.log("Medium Size", mediumSize());
+console.log("Large Size", largeSize());
+console.log("Extra Large Size", xLargeSize());
