31c25a222f2ea740333b81e1661919ec8e22bf2e
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-08-18 15:13:16 -0400

    Completed Object constructors lesson

--- Exercise Files/04_10/script.js
+++ Exercise Files/04_10/script.js
@@ -1,14 +1,36 @@
-var course = new Object();
+//var course = new Object();
+//
+//var course = {
+//    title: "JavaScript Essential Training",
+//    instructor: "Morten Rand-Hendriksen",
+//    level: 1,
+//    published: true,
+//    views: 0,
+//    updateViews: function() {
+//        return ++course.views;
+//    }
+//}
+//
+//console.log(course);
+//
+//var currentDate = new Date();
 
-var course = {
-    title: "JavaScript Essential Training",
-    instructor: "Morten Rand-Hendriksen",
-    level: 1,
-    published: true,
-    views: 0,
-    updateViews: function() {
-        return ++course.views;
+function Course(title, instructor, level, published, views) {
+    this.title = title;
+    this.instructor = instructor;
+    this.level = level;
+    this.published = published;
+    this.views = views;
+
+    this.updateViews = function() {
+        return ++this.views;
     }
 }
+    var courses = [
+        new Course("JavaScript Essential Training", "Morten Rand Hendriksen", 1, true, 0),
+        new Course("JavaScript rocks", "Ryan", 2, true, 957)
+    ];
 
-console.log(course);
+    console.log(courses);
+ 
+
