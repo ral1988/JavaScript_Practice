fdf804eccd89fe7813a822a7263f0a4278a361de
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-08-19 13:27:19 -0400

    fixed some text issues in the HTML files and setup the variables for the JS

--- Exercise Files/05_06/JS/script.js
+++ Exercise Files/05_06/JS/script.js
@@ -1,1 +1,11 @@
+const FEATURED = document.querySelector(".feature-image");
+const THEIMAGE = FEATURED.querySelector(".img"); //burrowing into DOM elements without having to traverse the entire DOM tree every time
+
+var altText = THEIMAGE.getAttribute("alt");
+
+var captionElement = document.createElement("figcaption");
+
+var captionText = document.createTextNode(altText);
+
+captionElement.appendChild(captionText);
 
