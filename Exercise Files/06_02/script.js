06a3ac7726316d7a4c22e57a3ed5a18c48764e70
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-08-21 10:49:50 -0400

    defined *position to equal current time and be in position inbetween times (3:35 vs 3:35:40). Very subtle, finer detail of positioning.

--- Exercise Files/06_02/script.js
+++ Exercise Files/06_02/script.js
@@ -2,17 +2,28 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPosition = 20;
-let minPosition = 130;
-let secPosition = 50;
+//let hrPosition = 20;
+//let minPosition = 130;
+//let secPosition = 50;
 
 var date = new Date();
 console.log(date);
 
+
 let hr = date.getHours();
 let min = date.getMinutes();
 let sec = date.getSeconds();
-console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+//console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+
+let secPosition = 360/60 * sec;
+let minPosition = (360/60 * min) + (secPosition/60);
+let hrPosition = (360/12 * hr) + (minPosition/60);
+
+
+
+
+
+
 
 
 
