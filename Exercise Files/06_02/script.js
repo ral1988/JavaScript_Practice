6e4f459ff245bd2e51e62eb9701176b1d1967555
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-08-21 11:08:09 -0400

    completion of clock exercise/project

--- Exercise Files/06_02/script.js
+++ Exercise Files/06_02/script.js
@@ -15,19 +15,20 @@ let min = date.getMinutes();
 let sec = date.getSeconds();
 //console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
-let secPosition = 360/60 * sec;
-let minPosition = (360/60 * min) + (secPosition/60);
-let hrPosition = (360/12 * hr) + (minPosition/60);
+let secPosition = 360 / 60 * sec;
+let minPosition = (360 / 60 * min) + (secPosition / 60);
+let hrPosition = (360 / 12 * hr) + (minPosition / 12);
+
+function clock() {
+
+    secPosition = secPosition + 6;
+    minPosition = minPosition + (6 / 60);
+    hrPosition = hrPosition + (3 / 360);
 
 
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
 
-
-
-
-
-
-
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
-
+var interval = setInterval(clock, 1000);
