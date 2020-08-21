40d508aac2d34a947b55e3732b9522407f9f9411
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-08-21 09:52:05 -0400

    inline css to an emlement class complete

--- Exercise Files/05_07/JS/script.js
+++ Exercise Files/05_07/JS/script.js
@@ -1,1 +1,9 @@
+//Inline CSS to an element
+document.querySelector(".cta a").style;
+
+document.querySelector(".cta a").style.color = "green"
+
+document.querySelector(".cta a").style.backgroundColor = "black";
+
+document.querySelector(".cta a").style.cssText = "padding: 1em; color: purple";
 
