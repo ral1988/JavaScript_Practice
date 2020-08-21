7125734ac0cc2bb495ad03dfa579f417910b568b
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-08-21 12:27:01 -0400

    broke up long lines of code and added some JS in script.js

--- Exercise Files/07_03/JS/script.js
+++ Exercise Files/07_03/JS/script.js
@@ -1,1 +1,17 @@
+const CTA = document.querySelector(".cta a");
+const ALERT = document.querySelector("#booking-alert");
+
+CTA.classList.remove("hide");;
+ALERT.classList.add("hide");
+
+function reveal(e) {
+
+    e.preventDefault();
+    CTA.classList.toogle("hide");
+    ALERT.classList.toogle("hide");
+
+}
+
+CTA.onclick = reveal;
+CTA.onclick = console.log("The button was clicked");
 
