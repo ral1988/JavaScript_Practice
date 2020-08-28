96fbaf7618dcb73111763bf7a50146d32de8c254
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-08-28 11:03:20 -0400

    Established a basic counter and have it start at 0

--- Exercise Files/08_02/script.js
+++ Exercise Files/08_02/script.js
@@ -4,12 +4,16 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+var timer = 0;
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 
 
 // Run a standard minute/second/hundredths timer:
-
+function runTimer() {
+    theTimer.innerHTML = timer;
+    timer++;
+}
 
 // Match the text entered with the provided text on the page:
 function spellCheck() {
@@ -20,6 +24,9 @@ function spellCheck() {
 // Start the timer:
 function start() {
     let textEnteredLength = testArea.value.length;
+    if (textEnteredLength === 0) {
+        setInterval(runTimer, 10);
+    }
     console.log(textEnteredLength);
 }
 
